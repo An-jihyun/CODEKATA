5a753047ad12da15de99fd46aa1828f85631833b
An-jihyun <76841930+An-jihyun@users.noreply.github.com>
2025-04-30 09:55:55 +0900

    [level 0] Title: 두 수의 합 구하기, Time: 0.02 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/0/120802. 두 수의 합 구하기/두 수의 합 구하기.java
+++ 프로그래머스/0/120802. 두 수의 합 구하기/두 수의 합 구하기.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int num1, int num2) {
         if(num1 < -50000 || num1 > 50000 || num2 < -50000 || num2 > 50000){
-            throw new IllegalArgumentException("입ㄹ력하는 숫자의 값은 -50000이상이면서 50000이하여야 합니다.");
+            throw new IllegalArgumentException("입력하는 숫자의 값은 -50000이상이면서 50000이하여야 합니다.");
         }
         return num1 + num2;
     }
